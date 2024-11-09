75e9e50c98ed824dd83530057061fe7221e0ebe1
Wegondim <Weprofissional@gmail.com>
2024-11-08 23:36:43 -0300

    Adicionar recursos ao aplicativo

--- MauiAppHotel/App.xaml.cs
+++ MauiAppHotel/App.xaml.cs
@@ -7,14 +7,14 @@ namespace MauiAppHotel
         {
             InitializeComponent();
 
-            MainPage = new AppShell();
+            MainPage = new NavigationPage(new Views.Sobre());
         }
 
         protected override Window CreateWindow(IActivationState? activationState)
         {
             var window = base.CreateWindow(activationState);
 
-            window.Width = 400;
+            window.Width = 400; 
             window.Height = 600;
 
             return window;
--- MauiAppHotel/MauiProgram.cs
+++ MauiAppHotel/MauiProgram.cs
@@ -13,10 +13,17 @@ namespace MauiAppHotel
                 {
                     fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
                     fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                    fonts.AddFont("Kalam-Bold.ttf", "KalamBold");
+                    fonts.AddFont("Kalam-Light.ttf", "KalamLight");
+                    fonts.AddFont("Kalam-Regular.ttf", "Kalam");
+                    fonts.AddFont("Mogra-Regular.ttf", "Mogra");
+                    fonts.AddFont("varelaround_regular.ttf", "varelaround");
+                    fonts.AddFont("SpaceGrotesk-VariableFont.ttf", "SpaceGrotesk");
+                    fonts.AddFont("PlaywriteGBS-Italic-VariableFont_wght.ttf", "Playwrt_italic");
+                    fonts.AddFont("PlaywriteGBS-VariableFont_wght.ttf", "Playwrt");
                 });
-
 #if DEBUG
-    		builder.Logging.AddDebug();
+            builder.Logging.AddDebug();
 #endif
 
             return builder.Build();
